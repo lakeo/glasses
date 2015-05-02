8fae84f5be3e430cb14bc8ba144f9c16d15b7f44
blue.xiaolu@gmail.com <blue.xiaolu@gmail.com>
2015-05-02 16:38:27 +0800

    add test

--- src/test/java/com/lxl/dao/TestDB.java
+++ src/test/java/com/lxl/dao/TestDB.java
@@ -1,14 +1,26 @@
 package com.lxl.dao;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
 
 /**
  * Created by xiaolu on 15/5/2.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath*:*.xml","classpath:*.xml"})
 public class TestDB {
+    @Resource
+    DfGroupMapper dfGroupMapper;
+
     @Test
     public void test()
     {
         System.out.println("hello");
+        Object o = dfGroupMapper.selectByPrimaryKey(1);
+        System.out.print(o);
     }
 }
